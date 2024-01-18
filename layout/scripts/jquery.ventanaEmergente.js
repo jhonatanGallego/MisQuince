8e53dbd20eec3d96a4c53232f92d38039afdee81
Jhonatan <jonathangallegotorres@gmail.com>
2024-01-18 16:28:46 -0500

    new file:   images/demo/backgrounds/corona.png 	new file:   images/demo/backgrounds/img_principal2.jpeg 	new file:   images/demo/backgrounds/img_principal2.jpg 	new file:   images/demo/backgrounds/img_principal3.png 	new file:   images/demo/gallery/Mis_cumples/photo22.jpeg 	new file:   images/demo/gallery/Mis_cumples/photo23.jpeg 	deleted:    images/demo/index.html 	new file:   images/videos/Hija_mia_e_tu_cumpleanos.m4a 	new file:   "images/videos/Que_se_cumplan_tus_sue\303\261os.m4a" 	modified:   inicio.html     modified:   layout/scripts/jquery.ventanaEmergente.js 	deleted:    layout/styles/index.html 	modified:   layout/styles/layout.css 	modified:   pages/MIs_disfraces.html 	modified:   pages/Mis_competencias.html 	modified:   pages/Mis_cumples.html 	modified:   pages/Primeros_meses.html

--- layout/scripts/jquery.ventanaEmergente.js
+++ layout/scripts/jquery.ventanaEmergente.js
@@ -1,29 +1,3 @@
-const btnAbrirLugar = document.querySelector("#btn-abrir-lugar");
-const btnCerrarlugar = document.querySelector("#btn-cerrar-lugar");
-const lugar = document.querySelector("#lugar");
-
-btnAbrirLugar.addEventListener("click",()=>{
-   lugar.showModal();
-})
-
-btnCerrarlugar.addEventListener("click",()=>{
-   lugar.close();
-})
-
-
-const btnAbrirRegalo = document.querySelector("#btn-abrir-regalo");
-const btnCerrarRegalo = document.querySelector("#btn-cerrar-regalo");
-const regalo = document.querySelector("#regalo");
-
-btnAbrirRegalo.addEventListener("click",()=>{
-   regalo.showModal();
-})
-
-btnCerrarRegalo.addEventListener("click",()=>{
-   regalo.close();
-})
-
-
 const btnAbrirVestir = document.querySelector("#btn-abrir-vestir");
 const btnCerrarVestir = document.querySelector("#btn-cerrar-vestir");
 const vestir = document.querySelector("#vestir");
@@ -34,43 +8,4 @@ btnAbrirVestir.addEventListener("click",()=>{
 
 btnCerrarVestir.addEventListener("click",()=>{
    vestir.close();
-})
-
-
-
-const btnAbrirFecha = document.querySelector("#btn-abrir-fecha");
-const btnCerrarFecha = document.querySelector("#btn-cerrar-fecha");
-const fecha = document.querySelector("#fecha");
-
-btnAbrirFecha.addEventListener("click",()=>{
-   fecha.showModal();
-})
-
-btnCerrarFecha.addEventListener("click",()=>{
-   fecha.close();
-})
-
-
-const btnAbrirFechaCita = document.querySelector("#btn-abrir-cita");
-const btnCerrarFechaCita = document.querySelector("#btn-cerrar-cita");
-const cita = document.querySelector("#cita");
-
-btnAbrirFechaCita.addEventListener("click",()=>{
-   cita.showModal();
-})
-
-btnCerrarFechaCita.addEventListener("click",()=>{
-   cita.close();
-})
-
-const btnAbrirMusica = document.querySelector("#btn-abrir-musica");
-const btnCerrarMusica = document.querySelector("#btn-cerrar-musica");
-const musica = document.querySelector("#musica");
-
-btnAbrirMusica.addEventListener("click",()=>{
-   musica.showModal();
-})
-
-btnCerrarMusica.addEventListener("click",()=>{
-   musica.close();
 })
